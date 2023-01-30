3b4761c6a725941f8cda3d4af355dfd6031dc64d
Ibrahim <ibrachar68@gmail.com>
2023-01-30 15:01:59 +0100

    fine creazione componente header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,77 @@
-<h1>Header</h1>
+
+<div class=header-top>
+
+</div>
+<header>
+    <div class="cont-header">
+      <div>
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}"class="img-header">
+      </div>
+      <ul>
+        <li class="list-header">
+          <a>
+            CHARACTERS
+          </a>
+
+        </li>
+        <li class="list-header">
+            <a>
+              COMICS
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+              MOVIES
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+              TV
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+              GAMES
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+             COLLECTIBLES
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+             VIDEOS
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+            FANS
+            </a>
+  
+          </li>
+          <li class="list-header">
+            <a>
+             NEWS
+            </a>
+  
+          </li>
+          <input placeholder="Search" type="text" />
+      </ul>
+    
+
+    </div>
+
+  </header>
+  <div class="jumbotron">
+    <img src="" alt="">
+    <a href="" class="text-current">CURRENT SERIES </a>
+</div>
+
+
